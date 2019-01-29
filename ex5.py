cb4962695c10b802cd4ae813927327332c45871b
Joe  Mackey <jmackey@scrapeskillet.com>
2019-01-29 09:57:11 -0600

    Create ex5.py
    Git is kicking my butt. I deleted ex5.py and trying to get it back again. This is a do over.

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = 'Joe A. Mackey'
+age = 69 # really
+height = 75 # inches
+weight = 285 # poundss and I am working on it
+eyes = 'blue'
+teeth = 'white' # depending on coffee
+hair = 'braown what there is of it'
+
+print(f"Lets talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f" He's {weight} poundss heavy. wow.")
+print("Actually tha is way too much")
+print(f"He's got {eyes} eyes and {hair}.")
+print (f"His teeth are usually (teeth} depending on the coffee.")
+
+#This line is tricky, try to get it exacally right. TYPING ID GOOD PRACTICE.
+total = age + height + weight
+print(f" If I add {age}, {height}, and {weight} I get {total}.")
