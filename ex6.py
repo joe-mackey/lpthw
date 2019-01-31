efb569b7f9eec2f1d8e9d78cf1472f5bfac2248b
Joe  Mackey <jmackey@scrapeskillet.com>
2019-01-31 10:36:30 -0600

    Update ex6.py
    struggling learning how to write  in VS then push to here.

--- ex6.py
+++ ex6.py
@@ -1,20 +1,22 @@
-x = ("There are %d types of people." %10)
-binary = ("binary")
-do_not = ("Don't")
-y = ("Those who know %s and those who %s." %(binary, do_not))
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
 
-print (x)
-print (y)
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
 
-print ("I said: %r." %x )
-print ("I also said: '%s'." %y)
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
 
 hilarious = False
-joke_evaluation = ("Is'nt that joke so funny?! %r")
+joke_evaluation = "Isn't that joke so funny?! {}"
 
-print (joke_evaluation % hilarious)
+print(joke_evaluation.format(hilarious))
 
-w = (" This is the left side of ...")
-e = ("a string with a right side")
+w = "This is the left side of..."
+e = "a string with a right side."
 
-print (w + e)
+print(w + e)
