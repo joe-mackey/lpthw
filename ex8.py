163d81c5fca88596198d06cb79ce3f00e8958c10
Joe  Mackey <jmackey@scrapeskillet.com>
2019-01-31 11:18:10 -0600

    Update ex8.py
    STILL HAVING TROUBLE SENDING UPDATES TO GIT HUB
    '

--- ex8.py
+++ ex8.py
@@ -1,13 +1,14 @@
-formatter = " {} {} {} {}"
+formatter = " {} {} {} {} {} {}"
 
-print (formatter.format (1, 2, 3, 4))
-print (formatter.format ("one", "two", "three", "four"))
-print (formatter.format (True, False, False, True))
-print (formatter.format (formatter, formatter, formatter, formatter))
+print (formatter.format (1, 2, 3, 4, 5, 6))
+print (formatter.format ("one", "two", "three", "four", "five", "six"))
+print (formatter.format (True, False, False, True, True, True))
+print (formatter.format (formatter, formatter, formatter, formatter , formatter, formatter))
 print (formatter.format (
     "Try your",
     "own text here",
     "Maybe a poem",
     "Or a song about fear",
-
+    "I fear sending this file",
+    "to github"
 ))
